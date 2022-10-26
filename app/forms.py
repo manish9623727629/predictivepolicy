091461d2d61f46daa9506997ccb3ba5685776186
manish9623727629 <110304343+manish9623727629@users.noreply.github.com>
2022-10-27 03:31:31 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -9,8 +9,8 @@ from wtforms.validators import DataRequired
 class InfoForm(FlaskForm):
     app_type = SelectField(u'Application Type',choices=['HPC','Non HPC'])
     resource = RadioField(u'Resources',choices=['CPU','GPU'],default='CPU')
-    cpu = SelectField(u'CPU Core',choices=[6,8,16,32,64])
-    gpu = SelectField(u'GPU',choices=[8,16,32,64])
+    cpu = SelectField(u'CPU Core',choices=[1,2,4,6,8,16,32,64])
+    gpu = SelectField(u'GPU',choices=[1,2,4,6,8,16,32,64])
     ram = IntegerField(u'RAM (GB)', validators=[DataRequired()])
     memory = IntegerField(u'Disk (GB)', validators=[DataRequired()])
     no_of_container = IntegerField(u'Number of container', validators=[DataRequired()])
